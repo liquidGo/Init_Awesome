d96780e8d1924b23e3f7f6da1d309598bdcd7703
左岸 <3112203373@163.com>
2024-03-15 19:19:25 +0800

    feat: Update Plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const base = require('./webpack.base.js');
 
 module.exports = merge(base, {
   mode: 'production',
   devtool: 'source-map',
   plugins:[
-    new CleanWebpackPlugin() 
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin() 
   ]
 });
